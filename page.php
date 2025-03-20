89833107a66e0db94e8d7a8335286990204b1c0a
rronig <gashi.1rron@gmail.com>
2025-03-20 18:53:46 +0100

    Initial commit

---
+++ page.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="home-blog">
+                        <div class="container">
+                            <div class="page-item">
+                                <?php
+                                    while( have_posts() ): the_post();
+                                        ?>
+                                            <article>
+                                                <h1><?php the_title(); ?></h1>
+                                                <?php the_content(); ?>
+                                            </article>
+                                        <?php
+                                    endwhile;
+                                ?>
+                            </div>
+                        </div>
+                    </section>
+                </main>
+            </div>
+        </div>
+<?php get_footer(); ?>
