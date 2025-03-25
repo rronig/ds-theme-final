36a98cc8dc3472275c24d5bdd5005fce1ef2fe6e
rronig <gashi.1rron@gmail.com>
2025-03-25 19:16:02 +0100

    a

--- functions.php
+++ functions.php
@@ -7,14 +7,68 @@ function wpdevs_load_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'wpdevs_load_scripts' );
 
-function dstheme_config(){
 register_nav_menus(
     array(
         'wp_devs_main_menu' => 'Main Menu',
         'wp_devs_footer_menu' => 'Footer Menu'
     )
 );
+function dstheme_config(){
 $args=array('height'=>225, 'width'=>1920);
 add_theme_support('custom-header', $args);
 add_theme_support('post-thumbnails');
+add_theme_support('custom-logo', array(
+    'width' => 200,
+    'height' => 110,
+    'flex-height' => true,
+    'flex-width' => true
+));
 }
+add_action('after_setup_theme','dstheme_config',0);
+add_action('widgets_init', 'dstheme_sidebars');
+function dstheme_sidebars(){
+    register_sidebar(
+        array(
+            'name' => 'Blog Sidebar',
+            'id' => 'sidebar-blog',
+            'description' => 'This is the Blog Sidebar. You can add your widgets here.',
+            'before_widget' => '<div class = "widget-title">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class = "widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name' => 'Service Sidebar 1',
+            'id' => 'services-1',
+            'description' => 'First Service Area',
+            'before_widget' => '<div class = "widget-title">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class = "widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name' => 'Service Sidebar 2',
+            'id' => 'services-2',
+            'description' => 'Second Service Area.',
+            'before_widget' => '<div class = "widget-title">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class = "widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+    register_sidebar(
+        array(
+            'name' => 'Service Sidebar 3',
+            'id' => 'services-3',
+            'description' => 'Third Service Area.',
+            'before_widget' => '<div class = "widget-title">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class = "widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+} 
